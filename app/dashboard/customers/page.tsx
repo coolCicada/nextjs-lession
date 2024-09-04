192315295ee4374ee4f7c5df00d46fea16e86a4b
梁赛 <liangsai.0@bytedance.com>
2024-09-04 15:36:57 +0800

    feat: update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -3,10 +3,10 @@
 import { useEffect } from "react";
 
 export default function Page() {
-  console.log('customers!!')
+	console.log('customers!!')
 
-  useEffect(() => {
-    console.log('eff customers') 
-}, []);
-  return <p>cust</p>;
+	useEffect(() => {
+		console.log('eff customers')
+	}, []);
+	return <p>cust</p>;
 }
