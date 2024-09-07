5cae20a3a2695acdb62184b6f5380855938c5c11
coolCicada <1475395755@qq.com>
2024-09-07 23:06:08 +0800

    feat: 完成hero 模块

--- app/nick/page.tsx
+++ app/nick/page.tsx
@@ -7,24 +7,6 @@ const App = () => (
         <section className="xl:padding-l wide:padding-r padding-b">
             <Hero />
         </section>
-        <section className="padding">
-            PopularProducts
-        </section>
-        <section className="padding">
-            SuperQuality
-        </section>
-        <section className="padding">
-            Services
-        </section>
-        <section className="padding">
-            SpecialOffer
-        </section>
-        <section className="padding bg-pale-blue">
-            CustomerReviews
-        </section>
-        <section className="padding-x sm:py-32 py-16 w-full">
-            Subscribe
-        </section>
         <section className="bg-black padding-x padding-t pb-8"></section>
     </main>
 )
---
+++ declaration.d.ts
@@ -0,0 +1,26 @@
+// 在项目根目录或 src 目录下创建 declaration.d.ts 文件
+declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const value: string;
+    export default value;
+  }
+  
