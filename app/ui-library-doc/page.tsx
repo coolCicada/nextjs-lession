2af0c21932cccc5e5e817757454291bffa02c0eb
梁赛 <liangsai.0@bytedance.com>
2024-10-12 16:21:18 +0800

    feat: 展示组件文档

---
+++ app/ui-library-doc/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Doc from 'simply-ui-docs';
+import 'simply-ui-docs/dist/style.css';
+
+const Page = () => {
+  return (
+    <div><Doc /></div>
+  )
+}
+
+export default Page;
