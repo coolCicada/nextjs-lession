894abc415fa396d9d87aa2f2a3406f1d550e2fe8
coolCicada <1475395755@qq.com>
2024-10-23 17:14:19 +0800

    feat: 个人首页简单适配移动端

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ const Page = () => {
   const router = useRouter();
   return (
     <div className='min-h-screen flex flex-col'>
-      <main>
-        <div className='mx-auto my-4  flex w-96 h-12 bg-slate-200 justify-around items-center rounded-full'>
+      <main className='px-4'>
+        <div className='mx-auto my-4  flex sm:w-96 h-12 bg-slate-200 justify-around items-center rounded-full'>
           <span>Home</span>
           <span>About</span>
           <span>Projects</span>
@@ -25,8 +25,8 @@ const Page = () => {
           <div className='text-sm text-center'>Hi, my name is</div>
           <div className='text-4xl text-center'>Matt</div>
         </div>
-        <div onClick={() => router.push('/oauth')} className='mx-auto mb-2 w-[750px] flex rounded-lg'>
-          <div className='w-1/2 bg-yellow-50 p-6'>
+        <div onClick={() => router.push('/oauth')} className='mx-auto mb-2 max-w-screen-md flex flex-col sm:flex-row rounded-lg overflow-hidden'>
+          <div className='bg-yellow-50 p-6 flex-1'>
             <div className='text-2xl'>oauth2.0</div>
             <div className='flex space-x-2 mt-2 mb-4'>
               <div>code</div>
@@ -36,7 +36,7 @@ const Page = () => {
               这是我oauth2.0的学习,来源于阮一峰老师的博客
             </div>
           </div>
-          <div className='bg-zinc-50'></div>
+          <div className='bg-red-400 flex-1'>12</div>
         </div>
         <div onClick={() => router.push('/ui-library-doc')} className='mr-2 mb-2 w-32 h-32 bg-slate-300 border flex justify-center items-center rounded-lg'>
           UI 组件库文档
