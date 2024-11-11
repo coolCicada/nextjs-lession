cd684c95891d8d3728aa9b1c806062d4076cd832
coolCicada <1475395755@qq.com>
2024-11-11 23:58:12 +0800

    feat: 优化主页 添加 loading

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   const router = useRouter();
   return (
     <div className='min-h-screen flex flex-col'>
-      <main className='px-4'>
+      <main className='px-4 flex-1'>
         <div className='mx-auto my-4  flex sm:w-96 h-12 bg-slate-200 justify-around items-center rounded-full'>
           <span>Home</span>
           <span>About</span>
@@ -25,33 +25,78 @@ const Page = () => {
           <div className='text-sm text-center'>Hi, my name is</div>
           <div className='text-4xl text-center'>Matt</div>
         </div>
-        <div onClick={() => router.push('/oauth')} className='mx-auto mb-2 max-w-screen-md flex flex-col sm:flex-row rounded-lg overflow-hidden'>
-          <div className='bg-yellow-50 p-6 flex-1'>
-            <div className='text-2xl'>oauth2.0</div>
+        <div className='flex flex-wrap justify-evenly gap-5 sm:flex-row rounded-xl overflow-hidden'>
+          <div className='bg-yellow-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/oauth')} className='text-2xl cursor-pointer hover:text-blue-300'>oauth2.0</div>
             <div className='flex space-x-2 mt-2 mb-4'>
               <div>code</div>
               <div>visit</div>
             </div>
             <div className='text-sm'>
-              这是我oauth2.0的学习,来源于阮一峰老师的博客
+              这是我oauth2.0的学习,来源于阮一峰老师的博客ewfwf范围分为非服务恶妇
             </div>
           </div>
-          <div className='bg-red-400 flex-1'>12</div>
-        </div>
-        <div onClick={() => router.push('/ui-library-doc')} className='mr-2 mb-2 w-32 h-32 bg-slate-300 border flex justify-center items-center rounded-lg'>
-          UI 组件库文档
-        </div>
-        <div onClick={() => router.push('/nick')} className='mr-2 mb-2 w-32 h-32 bg-slate-300 border flex justify-center items-center rounded-lg'>
-          nick
-        </div>
-        <div onClick={() => router.push('/login')} className='mr-2 mb-2 w-32 h-32 bg-slate-300 border flex justify-center items-center rounded-lg'>
-          login
-        </div>
-        <div onClick={() => router.push('/tailwindcss')} className='mr-2 mb-2 w-32 h-32 bg-slate-300 border flex justify-center items-center rounded-lg'>
-          tailwindcss
+          <div className='bg-pink-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/ui-library-doc')} className='text-2xl cursor-pointer hover:text-blue-300'>simply-ui</div>
+            <div className='flex space-x-2 mt-2 mb-4'>
+              <div>code</div>
+              <div>visit</div>
+            </div>
+            <div className="text-sm">
+              UI 组件库文档
+            </div>
+          </div>
+          <div className='bg-pink-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/nick')} className='text-2xl cursor-pointer hover:text-blue-300'>
+              Nick
+            </div>
+            <div className='flex space-x-2 mt-2 mb-4'>
+              <div>code</div>
+              <div>visit</div>
+            </div>
+            <div className="text-sm">
+              nick
+            </div>
+          </div>
+          <div className='bg-pink-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/login')} className='text-2xl cursor-pointer hover:text-blue-300'>
+              login
+            </div>
+            <div className='flex space-x-2 mt-2 mb-4'>
+              <div>code</div>
+              <div>visit</div>
+            </div>
+            <div className="text-sm">
+              login
+            </div>
+          </div>
+          <div className='bg-pink-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/ui-library-doc')} className='text-2xl cursor-pointer hover:text-blue-300'>
+              simply-ui
+            </div>
+            <div className='flex space-x-2 mt-2 mb-4'>
+              <div>code</div>
+              <div>visit</div>
+            </div>
+            <div className="text-sm">
+              UI 组件库文档
+            </div>
+          </div>
+          <div className='bg-pink-50 p-6 w-full sm:w-96'>
+            <div onClick={() => router.push('/tailwindcss')} className='text-2xl cursor-pointer hover:text-blue-300'>
+              tailwindcss
+            </div>
+            <div className='flex space-x-2 mt-2 mb-4'>
+              <div>code</div>
+              <div>visit</div>
+            </div>
+            <div className="text-sm">
+              tailwindcss 
+            </div>
+          </div>
         </div>
       </main>
-      <footer>
+      <footer className='flex justify-center'>
         <a href="https://beian.miit.gov.cn/">京ICP备19043673号-2</a>
       </footer>
     </div>
